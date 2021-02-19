9e09bfdac7e7c43dcf32ca901babc7d12a782532
Sricharan Ramesh <charan1952001@gmail.com>
2021-02-19 12:56:54 +0530

    /login and /register style

--- src/component/login.js
+++ src/component/login.js
@@ -2,25 +2,19 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div className="container login-page">
-        <div className="row">
-        <div className="col-md-6">
-            <div className="card">
-                <form className="box">
-                    <h1>Login</h1>
-                    <p className="text-muted"> Please enter your login and password!</p> 
-                    <input type="text" name="" placeholder="Username" /> 
-                    <input type="password" name="" placeholder="Password" /> 
-                    <a className='submit' name="" value="Login" href="/" >SUBMIT</a>
-                    <div className="col-md-12">
-                        <p className="text-muted"> New here? Sign Up</p> 
-                        <a className='submit btn btn-success' href="/register" >Sign Up</a>
-                    </div>
-                </form>
-            </div>
+        <div className="box">
+            <form>
+                <h1>Login</h1>
+                <p className="text-muted"> Please enter your login and password!</p> 
+                <input type="text" name="" placeholder="Username" /> 
+                <input type="password" name="" placeholder="Password" /> 
+                <a className='submit' name="" value="Login" href="/" >SUBMIT</a>
+                <div className="col-md-12">
+                    <p className="text-muted"> New here? Sign Up</p> 
+                    <a className='submit btn btn-success' href="/register" >Sign Up</a>
+                </div>
+            </form>
         </div>
-    </div>
-</div>
     )
 }
 
--- src/component/register.js
+++ src/component/register.js
@@ -2,25 +2,19 @@ import React from 'react'
 
 const Register = () => {
     return (
-        <div className="container login-page">
-        <div className="row">
-        <div className="col-md-6">
-            <div className="card">
-                <form className="box">
-                    <h1>Sign Up</h1>
-                    <input type="text" name="" placeholder="Full Name" />
-                    <input type="text" name="" placeholder="Username" /> 
-                    <input type="password" name="" placeholder="Password" /> 
-                    <a className='submit' name="" value="Login" href="/" >SUBMIT</a>
-                    <div className="col-md-12">
-                        <p className="text-muted">Log In?</p> 
-                        <a className='submit btn btn-success' href="/login" >Login</a>
-                    </div>
-                </form>
-            </div>
+        <div className="box">
+            <form >
+                <h1>Sign Up</h1>
+                <input type="text" name="" placeholder="Full Name" />
+                <input type="text" name="" placeholder="Username" /> 
+                <input type="password" name="" placeholder="Password" /> 
+                <a className='submit' name="" value="Login" href="/" >SUBMIT</a>
+                <div className="col-md-12">
+                    <p className="text-muted">Log In?</p> 
+                    <a className='submit btn btn-success' href="/login" >Login</a>
+                </div>
+            </form>
         </div>
-    </div>
-</div>
     )
 }
 
